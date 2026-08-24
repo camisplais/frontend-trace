ea574b2266c9a7aabacde7bf04c1a749da08316a
zanwonzzz <etnicastro9@gmail.com>
2026-08-23 19:40:06 -0600

    feat: changes vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    // El backend habilita CORS solo para http://localhost:5000,
-    // por eso fijamos el dev server en ese puerto.
-    port: 5000,
-    strictPort: true,
-  },
+    server: {
+      host: env.VITE_HOST,
+      port: Number(env.VITE_PORT),
+    },
+  }
 })
